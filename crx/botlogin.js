23c0ef6bb948bd70c15bf0d4e82f9a039c39b925
Theodore Dubois <tblodt@icloud.com>
2017-04-25 18:55:56 -0700

    Add fancy dist thing and fix things

---
+++ crx/botlogin.js
@@ -0,0 +1,1 @@
+../botlogin.js
--- takeover.js
+++ takeover.js
@@ -9,11 +9,20 @@ export function interceptSend(op, data) {
 export function interceptReceive(op, data, type) {
     if (op === 0 && type === "READY" && data.user.bot) {
         delete data.user.bot;
+        data.user.email = "fake@email";
+        // don't deny admin permissions because mfa isn't enabled
+        data.user.mfa_enabled = true;
         data.read_state = [];
         data.user_guild_settings = [];
-        data.tutorial = null;
-        data.friend_suggestion_count = 0;
-        data.notes = [];
         data.connected_accounts = [];
+        data.experiments = [];
     }
 }
+
+// friendSuggestionCount: t.friend_suggestion_count,
+// presences: t.presences || [],
+// notes: t.notes,
+// analyticsToken: t.analytics_token,
+// experiments: t.experiments,
+// connectedAccounts: t.connected_accounts,
+// guildExperiments: t.guild_experiments
